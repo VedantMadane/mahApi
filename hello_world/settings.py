a4c3a1488a225a9bfdc50f871a540770b8849e8d
Vedant Madane <6527493+VedantMadane@users.noreply.github.com>
2025-01-21 04:34:41 +0000

    Initial commit

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -44,7 +44,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_browser_reload",
+        "rest_framework",
 ]
+
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
